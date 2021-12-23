3eebec1b1bc08aee7dd2c6865cb10f755a971fa0
Pierre B <pierre.bouffier05@gmail.com>
2021-12-24 00:16:00 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -26,7 +26,7 @@ php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/bin/composer
 
 # Install Jetbrains Toolbox <3
-wget wget https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-1.22.10740.tar.gz -P /tmp
+wget wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.22.10970.tar.gz -P /tmp
 tar -zxf /tmp/jetbrains-toolbox-*.tar.gz -C ~/
 mv ~/jetbrains-toolbox-* ~/jetbrains-toolbox
 
